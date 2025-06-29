424f99d303b774aff809251057f833540f3952c1
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-06-29 18:10:22 +0545

    password_Reset djagno

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import *
 urlpatterns = [
     path('login/',Log_in.as_view(),name="log_in"),
     path('register1/',Register1.as_view(),name="register1"),
     path('Logout/',log_out,name="log_out"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
